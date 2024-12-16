56fb4838a55b7b2dc19a3a9431efb513ee3b999c
Martenda <lucasmartendal777@gmail.com>
2024-12-16 14:15:23 -0300

    Implemented LSTM Layers on the Model building.

--- main/train/build_model.py
+++ main/train/build_model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, LSTM, Reshape
 
 def create_model(input_shape, num_classes):
     model = Sequential()
@@ -16,12 +16,34 @@ def create_model(input_shape, num_classes):
     model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 1)))  # Pooling only over height
     model.add(Dropout(0.25))
-    
+    ### - ###
+
+    ### Reshaping data for the following layers ###
+    # Flattening and Reshaping
+    model.add(Flatten()) # Convert 2D to 1D vector
+    num_features = model.output_shape[1] // 1
+
+    # Reshape to make it suitable for LSTM layers
+    model.add(Reshape((1, num_features)))
+    ### - ###
+
+    # Hint: Use ConvLSTM2D...
+    ### LSTM layers ###    
+    # model.add(Dense(512, activation='relu'))
+    # model.add(Dropout(0.5))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(LSTM(512))
+    ### - ###
+
+    ### Fully connected layers ###
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
+    ### - ###
     
     model.add(Dense(num_classes, activation='softmax'))
     
